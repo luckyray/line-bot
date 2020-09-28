a1c69d255d2077e488035e825072d333d69aeeb9
Raymond Liu <wfliu66@qq.com>
2020-09-28 13:53:53 +0100

    update

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	msg = event.message.text
-	s = 'my name is ray robot!'
+	s = 'my name is raybot!'
 	line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s))
