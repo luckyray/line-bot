0a66f857e99b652c516a8f2fce2152148aa6f7ca
Raymond Liu <wfliu66@qq.com>
2020-09-28 05:28:35 +0100

    update

--- app.py
+++ app.py
@@ -37,9 +37,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+	msg = event.message.text
+	s = 'hello world!'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='hello world!'))
 
 
 if __name__ == "__main__":
